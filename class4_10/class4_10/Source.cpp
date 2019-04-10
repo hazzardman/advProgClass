4a297d13a16e7d9742fd4be1a736a5b72fb6e849
hazzardman <46685317+hazzardman@users.noreply.github.com>
2019-04-10 14:59:06 -0500

    a

--- class4_10/class4_10/Source.cpp
+++ class4_10/class4_10/Source.cpp
@@ -20,7 +20,6 @@ int main()
 	ifstream file;
 	file.open(filename);
 	
-	int x[3];
 	if (file.fail())
 	{
 		printf("file not found\n");
@@ -28,26 +27,48 @@ int main()
 	else 
 	{
 		file >> size;
-		for (int i = 0; !file.fail() || i < size; i++)
+		for (int i = 0; !file.fail() && i < size; i++)
 		{
 			file >> names[i] >> schools[i] >> medals[i];
 		}
 		file.close();
 	
-		map<string, int[3]> ranking;
+		map<string, int> gRanking;
+		map<string, int> sRanking;
+		map<string, int> bRanking;
 		for (int i = 0; i < size; i++)
 		{
-			ranking.insert(pair<string, int[3]>(schools[i], x));
-		
+			gRanking.insert(pair<string, int>(schools[i], 0));
+			sRanking.insert(pair<string, int>(schools[i], 0));
+			bRanking.insert(pair<string, int>(schools[i], 0));
 		}
-		map<string, string>::iterator m;
-		for (m = ranking.begin(); m != ranking.end(); m++)
-			cout << m->first << " -> "
-			<< m->second << "\n";
+		for (int i = 0; i < size; i++)
+		{
+			if (medals[i] == "gold")
+				gRanking[schools[i]]++;
 
+			else if (medals[i] == "silver")
+				sRanking[schools[i]]++;
+
+			else if (medals[i] == "bronze")
+				bRanking[schools[i]]++;
+		}
+		ofstream file2;
+		file2.open("ranking.txt");
+		file2 << "school\tgold\tsilver\tbronze\n";
+		map<string, int>::iterator m,n,o;
+		m = gRanking.begin();
+		n = sRanking.begin();
+		o = bRanking.begin();
+		for (; m != gRanking.end(); )
+		{
+			file2 << m->first << "\t" << m->second << "\t" << n->second << "\t" << o->second << "\n";
+			m++;
+			n++;
+			o++;
+		}
 	}
 	
-	getchar();
 	
 
 	return 0;
