a9301bbed7a6b01045f78fdd44648a0195321ca3
hazzardman <46685317+hazzardman@users.noreply.github.com>
2019-04-15 14:01:37 -0500

    a

--- class4_10/class4_10/Source.cpp
+++ class4_10/class4_10/Source.cpp
@@ -5,72 +5,88 @@
 #include<map>
 #include <fstream> 
 using namespace std;
-
+struct MedalCount
+{
+	int gold = 0;
+	int silver = 0;
+	int bronze = 0;
+};
 
 
 
 int main()
 {
-	int size = 100;
-	vector<string> names(size);
-	vector<string> schools(size);
-	vector<string> medals(size);
+	
+	vector<string> names;
+	vector<string> schools;
+	vector<string> medals;
 	string filename = "data.txt";
 	
 	ifstream file;
+	/*
+	//request filename
+	cout << "input file name of data: ";
+	cin >> filename;
+	*/
 	file.open(filename);
-	
+	//check for file
 	if (file.fail())
 	{
 		printf("file not found\n");
 	}
 	else 
-	{
+		//read file info into vectors
+	{	int size;
 		file >> size;
 		for (int i = 0; !file.fail() && i < size; i++)
 		{
-			file >> names[i] >> schools[i] >> medals[i];
+			string tmp1;
+			string tmp2;
+			string tmp3;
+			file >> tmp1 >> tmp2 >> tmp3;
+			names.push_back(tmp1);
+			schools.push_back(tmp2);
+			medals.push_back(tmp3);
 		}
 		file.close();
-	
-		map<string, int> gRanking;
-		map<string, int> sRanking;
-		map<string, int> bRanking;
+		//fill all school names into map for each medal
+		map<string, MedalCount*> Ranking;
 		for (int i = 0; i < size; i++)
 		{
-			gRanking.insert(pair<string, int>(schools[i], 0));
-			sRanking.insert(pair<string, int>(schools[i], 0));
-			bRanking.insert(pair<string, int>(schools[i], 0));
+			MedalCount* p = new MedalCount;
+			Ranking.insert(pair<string, MedalCount*>(schools[i], p));
+	
 		}
+		//asign medal counts for the schools
 		for (int i = 0; i < size; i++)
 		{
 			if (medals[i] == "gold")
-				gRanking[schools[i]]++;
+				Ranking[schools[i]]->gold++;
 
 			else if (medals[i] == "silver")
-				sRanking[schools[i]]++;
+				Ranking[schools[i]]->silver++;
 
 			else if (medals[i] == "bronze")
-				bRanking[schools[i]]++;
+				Ranking[schools[i]]->bronze++;
 		}
+		//print to file the medals awarded per school
 		ofstream file2;
 		file2.open("ranking.txt");
 		file2 << "school\tgold\tsilver\tbronze\n";
-		map<string, int>::iterator m,n,o;
-		m = gRanking.begin();
-		n = sRanking.begin();
-		o = bRanking.begin();
-		for (; m != gRanking.end(); )
+		map<string, MedalCount*>::iterator m;
+		
+		for (m = Ranking.begin(); m != Ranking.end(); m++)
 		{
-			file2 << m->first << "\t" << m->second << "\t" << n->second << "\t" << o->second << "\n";
-			m++;
-			n++;
-			o++;
+			file2 << m->first << "\t" << m->second->gold << "\t" << m->second->silver << "\t" << m->second->bronze << "\n";
+			
+			
 		}
 		file2.close();
+	
+	
+	
+	
+	
 	}
-	
-	
-
 	return 0;
 }
