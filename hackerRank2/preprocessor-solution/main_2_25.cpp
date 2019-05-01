28ff60752c30e7380c7aea41449d9abc6371408e
hazzardman <46685317+hazzardman@users.noreply.github.com>
2019-05-01 14:15:20 -0500

    a

---
+++ hackerRank2/preprocessor-solution/main_2_25.cpp
@@ -0,0 +1,38 @@
+/* Enter your macros here */
+
+#define toStr(a) #a
+#define io(v) cin >> v
+#define FUNCTION(name, op) void name(int &best, int q) { if (q op best)best = q; }
+#define INF 10000000
+#define foreach(a,b) for(int b=0;b<a.size();b++)
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+	FUNCTION(maximum, >)
+
+	int main() {
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result = ) << ' ' << ans;
+	return 0;
+
+}
